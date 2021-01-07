747e0d9a6c5ad71e552051838ecf6e56b9c213a5
Rene Wang <rene.wang1@hotmail.com>
2021-01-07 21:05:11 +0000

    fixed bug where file was not returning reservations correctly

--- 1_queries/05_all_my_reservations.sql
+++ 1_queries/05_all_my_reservations.sql
@@ -1,9 +1,9 @@
-SELECT properties.id, properties.title, properties.cost_per_night, reservations.start_date, AVG(property_reviews.rating) as average_rating
+SELECT properties.id, properties.title, properties.cost_per_night, reservations.start_date, avg(rating) as average_rating
 FROM reservations
-INNER JOIN properties ON properties.id = property_id
-INNER JOIN property_reviews ON reservations.id = reservation_id
+JOIN properties ON reservations.property_id = properties.id
+JOIN property_reviews ON properties.id = property_reviews.property_id 
 WHERE reservations.guest_id = 1
-AND end_date < now()::date
-GROUP BY reservations.id, properties.id
-ORDER BY start_date ASC
+AND reservations.end_date < now()::date
+GROUP BY properties.id, reservations.id
+ORDER BY reservations.start_date
 LIMIT 10;
